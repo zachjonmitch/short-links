f259f8fa07fbc801918441f4f534e96b3a917ae3
Zach Mitchell <zachjonmitch@gmail.com>
2021-08-29 23:59:53 -0600

    add error handling and frontend styling

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,14 +1,20 @@
 class LinksController < ApplicationController
 
-    def create
-        shortener = Shortener.new(link_params[:original_url])
-        @link = shortener.generate_short_link
-    end
+  def create
+    shortener = Shortener.new(link_params[:original_url])
+    @link = shortener.generate_short_link
 
-    private
-
-    def link_params
-        params.require(:link).permit(:original_url)
+    if @link.persisted?
+      respond_to :js
+    else
+      render 'error.js.erb'
     end
+  end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:original_url)
+  end
 
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -2,40 +2,48 @@ require 'rails_helper'
 
 RSpec.describe Link, type: :model do
 
-    it "is valid if it has an original URL and a lookup code" do
-        link = Link.new(
-            original_url: "https://www.favoritewebsite.com/articles/how-to-cook",
-            lookup_code: "1234567"
-        )
-        expect(link.valid?).to be(true)
-    end
-    
-    it "is invalid does not have a lookup code" do
-        link = Link.new(
-            original_url: "https://www.favoritewebsite.com/articles/how-to-cook"
-        )
-        expect(link.valid?).to be(false)
-    end
+  it "is valid if it has an original URL and a lookup code" do
+    link = Link.new(
+      original_url: "https://www.favoritewebsite.com/articles/how-to-cook",
+      lookup_code: "1234567"
+    )
+    expect(link.valid?).to be(true)
+  end
 
-    it "is invalid if it does not have an original URL" do
-        link = Link.new(
-            lookup_code: "1234567"
-        )
-        expect(link.valid?).to be(false)
-    end
+  it "is invalid if the URL is not formatted properly" do
+    link = Link.new(
+      original_url: "asdfasdfasfd",
+      lookup_code: "1234567"
+    )
+    expect(link.valid?).to be(false)
+  end
+  
+  it "is invalid does not have a lookup code" do
+    link = Link.new(
+      original_url: "https://www.favoritewebsite.com/articles/how-to-cook"
+    )
+    expect(link.valid?).to be(false)
+  end
 
-    it "is invalid if the lookup code already exists" do
-        link = Link.new(
-            original_url: "https://www.favoritewebsite.com/articles/how-to-cook",
-            lookup_code: "1234567"
-        )
-        link.save
+  it "is invalid if it does not have an original URL" do
+    link = Link.new(
+      lookup_code: "1234567"
+    )
+    expect(link.valid?).to be(false)
+  end
 
-        link_2 = Link.new(
-            original_url: "https://www.favoritewebsite.com/articles/how-to-bake",
-            lookup_code: "1234567"
-        )
-        expect(link_2.valid?).to be(false)
-    end
+  it "is invalid if the lookup code already exists" do
+    link = Link.new(
+      original_url: "https://www.favoritewebsite.com/articles/how-to-cook",
+      lookup_code: "1234567"
+    )
+    link.save
+
+    link_2 = Link.new(
+      original_url: "https://www.favoritewebsite.com/articles/how-to-bake",
+      lookup_code: "1234567"
+    )
+    expect(link_2.valid?).to be(false)
+  end
 
 end
