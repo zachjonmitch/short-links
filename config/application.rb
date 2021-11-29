b9ad2a8e94b05efb922b7b8491550f6084c4a5df
Zach Mitchell <zachjonmitch@gmail.com>
2021-11-29 01:33:14 -0700

    Add header

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module G2ShortLinks
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.enabled = false
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
