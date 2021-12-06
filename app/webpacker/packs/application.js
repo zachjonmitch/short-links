b97a4bebddb2f659a117fc11fa64754b7474f4d3
Zach Mitchell <zachjonmitch@gmail.com>
2021-12-05 23:46:27 -0700

    Add hmr and styles to webpacker

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -8,6 +8,8 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import '../styles/styles.scss'
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
+if (!module.hot) {
+  environment.loaders.get('sass').use.find(item => item.loader === 'sass-loader').options.sourceMapContents = false;
+}
+
+const config = environment.toWebpackConfig()
+config.output.filename = "js/[name]-[hash].js"
+
 module.exports = environment.toWebpackConfig()
